12a686975ef0fffbc6646c7359fd1c87f14cccde
799525644 <799525644@qq.com>
2021-12-23 16:00:46 +0800

    通过python实现socket

--- socket_test/socket_client.py
+++ socket_test/socket_client.py
@@ -4,8 +4,8 @@ import socket
 client = socket.socket()
 
 # 2.调用connect()连接ip和端口
-# 2.1 win和mac/linux 的终端中查询自己的ip地址，可以填127.0.0.1或查到的本地ipv4地址10.171.13.76
-# 2.2 虚拟机也会有额外的ip
+# win和mac/linux 的终端中查询自己的ip地址，可以填127.0.0.1或查到的本地ipv4地址10.171.13.76
+# 虚拟机也会有额外的ip
 client.connect(('127.0.0.1', 8000))
 
 # 3.1调用send()向服务器传一个数据，两种方式：常用.encode
@@ -20,8 +20,15 @@ client.connect(('127.0.0.1', 8000))
 
 # 3.3调用send()与recv()，实现发送端接收端双向通信，
 # 客户端接收一次进入服务消息；接收多次服务应答
-server_data = client.recv(1024)
-print("server response:{}".format(server_data.decode("utf8")))
+# server_data = client.recv(1024)
+# print("server response:{}".format(server_data.decode("utf8")))
+# while True:
+#     input_data = input()
+#     client.send(input_data.encode("utf8"))
+#     server_data = client.recv(1024)
+#     print("server response:{}".format(server_data.decode("utf8")))
+
+# 3.4多
 while True:
     input_data = input()
     client.send(input_data.encode("utf8"))
